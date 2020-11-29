8f8ca9046bc41ba91f5dccd1e7984ea4d6fdc241
Neo Anoman <42651314+NeoAnoman@users.noreply.github.com>
2020-11-29 15:14:03 +0530

    1st screen added

---
+++ components/MainComponent.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import styles from './styles/stylesheet.js'
+import { Text, View, Image } from 'react-native';
+
+export default function Start() {
+    return (
+        <View style={styles.container}>
+            <Image
+                source= {require('./images/logo1.png')} 
+                style= {styles.img_c}
+            />
+            <Text style= {styles.appfont}>Sanganaka Learning App</Text>
+        </View>
+    );
+}
+
---
+++ components/styles/stylesheet.js
@@ -0,0 +1,21 @@
+import { StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    img_c: {
+        height: 200,
+        width: 200,
+        marginTop: 250
+    },
+    appfont: {
+        color: '#064789',
+        fontSize: 20,
+        position: 'fixed',
+        bottom: 30
+    }
+});
