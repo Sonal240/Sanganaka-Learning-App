8cf426eea78fc73c1191ff658583190079092c1d
Sonal240 <simranshilky@gmail.com>
2020-12-01 16:41:56 +0530

    Welcome Screen Component Added

---
+++ components/styles/stylesheet.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Dimensions} from 'react-native';
+//const {windowWidth, windowHeight}= Dimensions.get('screen');
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    img_c: {
+        height: 200,
+        width: 200,
+        marginTop: 256
+    },
+    appfont: {
+        color: '#064789',
+        fontSize: 24,
+        position: 'fixed',
+        bottom: 32
+    },
+    carouselImg:{
+        height: '15rem',
+        width: '21rem',
+        marginTop: '12rem',
+        marginRight:'auto',
+        marginLeft:'auto',
+
+    },
+
+});
