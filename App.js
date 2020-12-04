d5e749cc07b5fca7406316a8d110c33de797622f
Sonal240 <simranshilky@gmail.com>
2020-12-04 23:01:50 +0530

    Sign Up ui component made backend left

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { View } from 'react-native';
 //import { Loading } from './components/LoadingComponent';
 //import Start from './components/MainComponent';
-import LogInScreen from './components/LoginPhoneNumber';
-
+//import LogInScreen from './components/LoginPhoneNumber';
+import SignUp from './components/SignUpComponent';
 export default function App() {
   return (
     <View>
-        <LogInScreen/>
+        <SignUp/>
     </View>
   );
 }
